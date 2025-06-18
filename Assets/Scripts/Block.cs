4ffd760dc648c106cefd6ecf487dd27b3e65bd75
LoncheDePierna <150973171+LoncheDePierna@users.noreply.github.com>
2025-06-17 18:21:44 -0600

    ananana

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -11,45 +11,40 @@ public class Block : MonoBehaviour
     private GameObject floorInstanceF;
     private GameObject floorInstanceS;
 
-    public bool FaceFront;
-    public bool FaceLeft;
-    public bool FaceRight;
-    public bool FaceBack;
+    public bool faceFront;
+    public bool faceLeft;
+    public bool faceRight;
+    public bool faceBack;
+
+    public bool floorFront;
+    public bool floorLeft;
+    public bool floorRight;
+    public bool floorBack;
 
     [Space(20)]
     public bool noFloor;
-
-    private void OnEnable()
-    {
-        GameManager.Instance.onRotate += HandleRotation;
-    }
-
     private void OnDisable()
     {
         GameManager.Instance.onRotate -= HandleRotation;
     }
-
     private void Start()
     {
+        floorInstanceF = Instantiate(floorPrefabF, transform);
+        floorInstanceF.transform.localPosition = new Vector3(0f, 0.395f, 0f);
+
+        floorInstanceS = Instantiate(floorPrefabS, transform);
+        floorInstanceS.transform.localPosition = new Vector3(0f, 0.395f, 0f);
+
+        GameManager.Instance.onRotate += HandleRotation;
+
         HandleRotation(GameManager.Instance.currentFace);
     }
 
     private void HandleRotation(GameManager.Face face)
     {
-        if (currentInstance != null)
-        {
-            Destroy(currentInstance);
-            Destroy(floorInstanceF);
-            Destroy(floorInstanceS);
-        }
+        if (currentInstance != null) Destroy(currentInstance);
 
         currentInstance = Instantiate(wallPrefab, transform);
-
-        floorInstanceF = Instantiate(floorPrefabF, transform);
-        floorInstanceF.transform.localPosition = new Vector3(0f,0.32f,0f);
-
-        floorInstanceS = Instantiate(floorPrefabS, transform);
-        floorInstanceS.transform.localPosition = new Vector3(0f, 0.32f, 0f);
 
         currentInstance.transform.localPosition = GetLocalPosition(face);
         currentInstance.transform.localRotation = Quaternion.Euler(GetLocalRotation(face));
@@ -67,14 +62,18 @@ public class Block : MonoBehaviour
             floorInstanceS.gameObject.SetActive(true);
         }
 
-        if ((face == GameManager.Face.Front && !FaceFront) ||
-            (face == GameManager.Face.Left && !FaceLeft) ||
-            (face == GameManager.Face.Right && !FaceRight) ||
-            (face == GameManager.Face.Back && !FaceBack))
+        if ((face == GameManager.Face.Front && !faceFront) ||
+            (face == GameManager.Face.Left && !faceLeft) ||
+            (face == GameManager.Face.Right && !faceRight) ||
+            (face == GameManager.Face.Back && !faceBack))
         {
             currentInstance.gameObject.SetActive(false);
         }
-        if (noFloor)
+
+        if ((face == GameManager.Face.Front && !floorFront) ||
+            (face == GameManager.Face.Left && !floorLeft) ||
+            (face == GameManager.Face.Right && !floorRight) ||
+            (face == GameManager.Face.Back && !floorBack))
         {
             floorInstanceF.gameObject.SetActive(false);
             floorInstanceS.gameObject.SetActive(false);
@@ -86,7 +85,7 @@ public class Block : MonoBehaviour
         switch (face)
         {
             case GameManager.Face.Front:
-                return new Vector3(0f, 0.2f, 0f);
+                return new Vector3(0.2f, 0.2f, 0f);
             case GameManager.Face.Left:
                 return new Vector3(0f, 0.2f, -0.2f);
             case GameManager.Face.Right:
